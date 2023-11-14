382ac09677bdabef86ac4889dffe6fe185c44272
kamrulhasan6 <142438937+kamrulhasan6@users.noreply.github.com>
2023-11-14 15:39:48 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let incrementBtn =document.querySelector('#increment');
+let decrementBtn =document.querySelector('#decrement');
+let input =document.querySelector('input');
+
+incrementBtn.addEventListener('click',function(){
+    input.value++;
+} )
+
+decrementBtn.addEventListener('click',function(){
+
+    if(input.value > 1){
+        input.value--;
+    }
+} )
